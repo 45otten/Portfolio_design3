2142d29a90747274e888a97b5af3058e124b7f87
Abdul Ahad . <abdulahad2a8@gmail.com>
2024-01-12 05:09:57 +0500

    uploaded icons in auto typing

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 var autotxt;
-function autotyping(){
-autotxt=new Typed(".about-me-string",{
-    strings:["with extensive skills and a grip over HTML 5,CSS and Javascript"],
-    loop:false,
-});
+function autotyping() {
+  autotxt = new Typed(".about-me-string", {
+    strings: [
+      "with extensive skills and a grip over <strong>HTML 5 <i class='fa-brands fa-html5'></i></strong>,<strong>CSS <i class='fa-brands fa-css3-alt'></i></strong> and <strong>Javascript <i class='fa-brands fa-js'></i></strong>",
+    ],
+    loop: false,
+  });
 }
 autotyping();
 
@@ -11,11 +13,11 @@ let banner = document.querySelector(".nav-bar");
 let navbar = document.querySelector(".nav-bar nav");
 
 function downloadFile() {
-    var fileUrl = 'assets/files/CV.txt';
-    var link = document.createElement('a');
-    link.href = fileUrl;
-    link.download = 'Web Designer Ahad_CV';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  var fileUrl = "assets/files/CV.txt";
+  var link = document.createElement("a");
+  link.href = fileUrl;
+  link.download = "Web Designer Ahad_CV";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
