aadd2b79dea8b267c810ca7ab29d73508e5845c6
45otten <abdulahad2a8@gmail.com>
2024-01-13 05:51:47 +0500

    files fixed

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let banner = document.querySelector(".nav-bar");
 let navbar = document.querySelector(".nav-bar nav");
 
 function downloadFile() {
-  var fileUrl = "assets/files/CV.txt";
+  var fileUrl = "/CV.txt";
   var link = document.createElement("a");
   link.href = fileUrl;
   link.download = "Web Designer Ahad_CV";
